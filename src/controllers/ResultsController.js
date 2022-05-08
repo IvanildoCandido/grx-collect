acbb6716b1fff9b79f5e5a31dc31c9210afd88c0
Ivanildo Cândido <systechtecnologia@gmail.com>
2022-05-08 01:21:10 -0300

    Add data to pages

--- src/controllers/ResultsController.js
+++ src/controllers/ResultsController.js
@@ -4,6 +4,26 @@ module.exports = {
   resultsCollect: async (req, res) => {
     let data = await getAllResults();
     let results = countQuantities(JSON.parse(data));
-    res.render("pages/results", { results });
+    console.log(results);
+    res.render("pages/results", {
+      total: results[4],
+      values: [
+        {
+          title: results[1],
+          value: results[5],
+          percent: ((results[5] * 100) / results[4]).toFixed(2),
+        },
+        {
+          title: results[2],
+          value: results[6],
+          percent: ((results[6] * 100) / results[4]).toFixed(2),
+        },
+        {
+          title: results[3],
+          value: results[7],
+          percent: ((results[7] * 100) / results[4]).toFixed(2),
+        },
+      ],
+    });
   },
 };
--- src/helpers/operations.js
+++ src/helpers/operations.js
@@ -18,7 +18,11 @@ module.exports = {
       { name: "totalPositive", value: totalPositive },
       { name: "totalNegative", value: totalNegative },
       { name: "totalNotEvaluated", value: totalNotEvaluated },
-    ];
-    return results.sort((a, b) => (a.value > b.value ? -1 : true));
+    ].sort((a, b) => (a.value > b.value ? -1 : true));
+    results = results.reduce((acc, item, index) => {
+      acc[Object.values(item)[0]] = Object.values(item)[1];
+      return acc;
+    }, {});
+    return [...Object.keys(results), ...Object.values(results)];
   },
 };
